67bbef8de68d2634809478439f68df6c22086050
pedrocosta <pedrohenriquecs012@gmail.com>
2026-06-08 11:51:11 -0300

    Criação do processo de busca de api

--- projects/segundo-app/src/App.jsx
+++ projects/segundo-app/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
+  const [nutri, setNutri] = useState([]);
 
+  useEffect(() => {
+    function loadApi() {
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+          setNutri(json);
+        });
+    }
+
+    loadApi();
+  }, []);
   return (
-    <h1>Segundo</h1>
-         
-  )
+    <div className="container">
+      <header>
+        <h1>React Nutri</h1>
+      </header>
+      <h1>TESTE</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
