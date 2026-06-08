20fd235635064fa2d9dc5c70184f2995e103732e
pedrocosta <pedrohenriquecs012@gmail.com>
2026-06-08 09:35:21 -0300

    Primeiro commit

---
+++ projects/primeiro-app/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [tarefas, setTarefas] = useState(["Pagar", "Conta"]);
+
+  function register(e) {
+    e.preventDefault();
+
+    if (input === ''){
+      alert("Coloque uma tarefa")
+    } else {
+      setTarefas([...tarefas, input]);
+      setInput('');
+    }
+  }
+
+  return (
+    <div>
+      <h1>Cadastrando usuário</h1>
+
+      <form onSubmit={register}>
+        <label>Nome: </label>
+        <br />
+        <input
+          placeholder="Digite uma tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <br />
+
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br />
+      <br />
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
